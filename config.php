7b698b6a2cd820dc51fddcc110e69c47cca3baef
Yashdeep Singh <yashdeepsingh@hotmail.com>
2017-04-06 01:18:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $smtphost = "smtp.gmail.com";
 $smtpuser = "yashdeep.singhdhingra@gmail.com";
-$smtppass = "palka123";
+$smtppass = "password";
 $secret = "6LdhUxsUAAAAANlKKu_tpIQhjZ625QSwBQbd-whU";
 ?>
